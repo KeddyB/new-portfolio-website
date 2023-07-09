14e0af166fd4a1ef68eacf5962761034dbeac671
KeddyB <bokelusi@gmail.com>
2023-07-09 16:08:42 +0100

    scroll reveal adds animation to selected objects

--- app.js
+++ app.js
@@ -39,3 +39,15 @@ window.onscroll = () =>{
    menuIcon.classList.remove("bx-x")
    navbar.classList.remove("active")
 }
+
+// scroll reveal
+
+ScrollReveal({
+   reset: true,
+   distance: "80px",
+   duration: 2000,
+   delay: 200
+})
+
+ScrollReveal().reveal('.home-content, .heading', {origin: 'left'})
+ScrollReveal().reveal('.home-img, .services, .portfolio-box, .contact form', {origin: 'right'})
